83dffc6ccf139b42c084493aebb19f32ab42ce15
Dimas <dimaszulfa0@gmail.com>
2023-01-11 22:46:56 +0700

    parent fragment manager & fragment stack

--- app/src/main/java/com/arcquila/androidpemula/belajarFragment/HelloFragment.kt
+++ app/src/main/java/com/arcquila/androidpemula/belajarFragment/HelloFragment.kt
@@ -5,18 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.arcquila.androidpemula.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HelloFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HelloFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -24,10 +16,7 @@ class HelloFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +24,25 @@ class HelloFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_hello, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HelloFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HelloFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnToFlex: Button = view.findViewById<Button>(R.id.btnFlex)
+
+        btnToFlex.setOnClickListener {
+            val mFlexibleFragment = FlexibleFragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().replace(R.id.fragment_container, mFlexibleFragment, FlexibleFragment::class.java.simpleName)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
